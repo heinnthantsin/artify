7b3058e67508a99671a05f38af6704e617062714
heinnthantsin <heinnthant01@gmail.com>
2024-05-13 02:39:27 +0630

    [fixed FollowingList Route]

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function Footer() {
     return (
-        <div className="fixed w-screen bottom-0 text-center py-2 text-quinary bg-forest">
+        <div className="w-screen bottom-0 text-center py-2 text-quinary bg-forest">
             <p>2024@Artify: Your Digital Marketplace</p>
         </div>
     )
